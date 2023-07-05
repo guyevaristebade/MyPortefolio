42fd7397224f943ff4feb0d4367d30ab78fea975
guyevaristebade <badeguyevariste@gmail.com>
2023-07-05 15:41:49 +0200

    Initial commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
+import { faBrands } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
         <div className="footer">
             <p className="footer-text-left">Designer et Developpé Par Guy EVariste Bade</p>
             <p className="footer-text-center">Copyright &copy; 2023</p>
-            <p className="footer-text-center">Copyright &copy; 2023</p>
+            <p className="footer-text-right">
+                <FontAwesomeIcon icon={faBrands} />
+            </p>
         </div>
     )
 }
+
+export default Footer
